eb1cd1907dcd4b2f55b1a620a17398fd28f6e208
YoonSoon Kang <kangyoonsoon@hotmail.com>
2020-05-05 02:42:55 -0400

    Filling Out the urls_index.ejs Template

--- express_server.js
+++ express_server.js
@@ -6,7 +6,8 @@ app.set("view engine", "ejs");
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  abc3dd: "http://www.naver.com"
 };
 
 app.get("/", (req, res) => {
@@ -15,6 +16,11 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  let templateVars = { greeting: "Hello World!" };
+  res.render("hello_world", templateVars);
 });
 
 app.get("/hello", (req, res) => {
@@ -29,6 +35,11 @@ app.get("/set", (req, res) => {
 app.get("/fetch", (req, res) => {
   const a = "Yo Chicken!!";
   res.send(`a = ${a}`);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { key: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
